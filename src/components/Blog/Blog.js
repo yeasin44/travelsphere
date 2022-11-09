6a9b0706a3e89bd9ff32a5155aceb2cb492018a4
yeasin44 <mohammadyeasinchy44@gmail.com>
2022-11-10 02:09:48 +0600

    first question added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,10 +2,28 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div className="max-w-screen-lg mx-auto">
-      <h2>Question & Answer</h2>
-      <div>
-        <h2>Difference between SQL and NoSQL?</h2>
+    <div className=" p-6 bg-lime-100">
+      <div className="max-w-screen-lg mx-auto">
+        <h2 className="text-center text-3xl font-semibold">
+          Question & Answer
+        </h2>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            1. What is the difference between SQL and NoSQL?
+          </div>
+          <div className="collapse-content">
+            <p>
+              ANS: SQL is the programming language used to interface with
+              relational databases. (Relational databases model data as records
+              in rows and tables with logical links between them). NoSQL is a
+              class of DBMs that are non-relational and generally do not use
+              SQL.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
