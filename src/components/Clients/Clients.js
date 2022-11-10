242f00ae4113dc6c3b43dfcd7a143b9327b28c48
yeasin44 <mohammadyeasinchy44@gmail.com>
2022-11-10 19:44:06 +0600

    clients added

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -5,7 +5,9 @@ import client2 from "../../assets/images/client-2.png";
 const Clients = () => {
   return (
     <div className="max-w-screen-lg mx-auto py-12">
-      <h2 className="text-center text-2xl p-6 font-bold">What my client say</h2>
+      <h2 className="text-center text-xl md:text-2xl lg:text-4xl p-6 font-bold">
+        What my client say
+      </h2>
       <div className="border-2 p-12 bg-slate-800 rounded-xl">
         <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-4 md:gap-20 gap-12 mt-6">
           <div className="border-2 rounded-xl border-slate-900 p-6 bg-white">
@@ -16,10 +18,9 @@ const Clients = () => {
             </div>
             <h2 className="text-center font-bold mb-2">Hasan Ali</h2>
             <p>
-              A tour guide or a tourist guide is a person who provides
-              assistance, information on cultural, historical and contemporary
-              heritage to people on organized sightseeing and individual clients
-              at educational{" "}
+              He is providing very good and safe service. You can rely and be
+              safe with him. He is very friendly and funny. I have had
+              experiences with other guide but he is really different one.
             </p>
           </div>
           <div className="border-2 rounded-xl border-slate-900 p-6 bg-white">
@@ -30,10 +31,11 @@ const Clients = () => {
             </div>
             <h2 className="text-center font-bold mb-2">Mohammad Imran</h2>
             <p>
-              A tour guide or a tourist guide is a person who provides
-              assistance, information on cultural, historical and contemporary
-              heritage to people on organized sightseeing and individual clients
-              at educational{" "}
+              I’m never really worried that the booth isn’t going to get there
+              on time. I can call over there and talk to anyone and get my
+              questions answered in an instant. Ease of getting a hold of
+              people. All communication with them is really easy. They do handle
+              everything.
             </p>
           </div>
         </div>
